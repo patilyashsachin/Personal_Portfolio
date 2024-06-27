adacf17ccf6180d680a88e4457f216290d3827f1
patilyashsachin <patilyash1914@gmail.com>
2024-06-27 21:21:53 +0530

    message

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import Main from './components/Main/Main'
 import Projects from './components/Project/Projects'
 import Contact from './components/Contact/Contact.js'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Main/>}/>
           <Route path='/Projects' element={<Projects/>} />
           <Route path='/Contact' element={<Contact/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
