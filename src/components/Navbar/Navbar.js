f9855b28b43554a22e493a3d0fd382c67e068c79
patilyashsachin <patilyash1914@gmail.com>
2024-08-06 23:58:10 +0530

    updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,39 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-scroll';
+import '../../App.css';
 
 function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light fw-bold sticky-top bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light fw-bold sticky-top bg-light">
         <div className="container">
-            <h3 style={{color:'#7755ff'}} className='fw-bold'>Yash's Portfolio</h3>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <h3 style={{ color: '#7755ff' }} className='fw-bold'>Yash's Portfolio</h3>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                <a className="nav-link active me-5" href="#AboutMe">About Me</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link active me-5" href="#Skill">Skills</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link active me-5" href="#Certification">Certification</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link active me-5" href="#Projects">Projects</a>
-                </li>
-                <button type="button" className="btn btn-warning fw-bold">
+              <li className="nav-item">
+                <Link 
+                  className="nav-link active me-5" 
+                  to="AboutMe" 
+                  smooth={true} 
+                  duration={100}
+                  style={{ cursor: 'pointer' }}>
+                  About Me
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link 
+                  className="nav-link active me-5" 
+                  to="Skill" 
+                  smooth={true} 
+                  duration={100}
+                  style={{ cursor: 'pointer' }}>
+                  Skills
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link 
+                  className="nav-link active me-5" 
+                  to="Certification" 
+                  smooth={true} 
+                  duration={100}
+                  style={{ cursor: 'pointer' }}>
+                  Certification
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link 
+                  className="nav-link active me-5" 
+                  to="Projects" 
+                  smooth={true} 
+                  duration={100}
+                  style={{ cursor: 'pointer' }}>
+                  Projects
+                </Link>
+              </li>
+              <button type="button" className="btn btn-warning fw-bold">
                 <a href='https://github.com/' target="_blank" id='new'>
                   Github
                 </a>
-                </button>
+              </button>
             </ul>
-            </div>
+          </div>
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
 export default Navbar;
