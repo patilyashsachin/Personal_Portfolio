c807cfe70a367e45180ca6c261b844e49422f65f
patilyashsachin <patilyash1914@gmail.com>
2024-08-07 16:29:55 +0530

    updated

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,7 +11,10 @@ function AboutMe() {
                         Over the course of my studies and personal projects, I have gained experience in programming language, i.e Java.
                         In addition to my technical expertise, I am always eager to learn and stay updated with the latest industry trends. I enjoy working on challenging projects that push me to grow and expand my skill set. <br />
                         </p>
-                        <a href={`${process.env.PUBLIC_URL}/Yash's Resume.pdf`} download="Yash's Resume.pdf"><button type="button" className="btn btn-warning fw-bold me-4 mt-3 py-2">Download CV</button></a>
+                        <a href={`${process.env.PUBLIC_URL}/Yash's Resume.pdf`} target="_blank" rel="noopener noreferrer">
+                            <button type="button" className="btn btn-warning fw-bold me-4 mt-3 py-2">View CV</button>
+                        </a>
+
                     </div>
                 </div>
             </div>
