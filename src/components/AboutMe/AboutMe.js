cb3b94e59fd8ad792c0d9306204e57c56e3d07e9
patilyashsachin <patilyash1914@gmail.com>
2025-07-04 14:16:47 +0530

    About

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,9 +8,9 @@ function AboutMe() {
             <div className="container py-5">
                     <div className="mt-3 px-5">
                         <h1 className='text-warning'>About Me</h1>
-                        <p className='text-light fw-light fs-5 mt-3'>Hello! My name is Yash, and I am currently pursuing a Bachelor of Engineering in Information Technology. I am passionate about web development and have developed skills in fullstack development, with a strong emphasis on frontend technologies. <br />
-                        Over the course of my studies and personal projects, I have gained experience in programming language, i.e Java.
-                        In addition to my technical expertise, I am always eager to learn and stay updated with the latest industry trends. I enjoy working on challenging projects that push me to grow and expand my skill set. <br />
+                        <p className='text-light fw-light fs-5 mt-3'>Hello! I'm Yash, a recent Bachelor of Engineering graduate in Information Technology, with a strong passion for web development and hands-on experience in fullstack development using the MERN stack (MongoDB, Express.js, React, Node.js).
+                        In addition to my web development skills, I have a solid foundation in Java programming, which has strengthened my problem-solving abilities and understanding of core programming concepts. Through academic projects and personal work, I've built and deployed complete web applications, continuously exploring new tools and best practices.
+                        I’m driven by curiosity, eager to keep learning, and motivated to contribute to impactful tech solutions. <br />
                         </p>
                         <a href={`${process.env.PUBLIC_URL}/Yash_Patil_Resume.pdf`} target="_blank" rel="noopener noreferrer">
                             <button type="button" className="btn btn-warning fw-bold me-4 mt-3 py-2">Resume</button>
